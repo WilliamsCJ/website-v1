463cf6f0c28d8eb25a0b9e5aef8957bb4816f8da
CJ Williams <cj@cjwilliams.io>
2020-05-15 13:06:29 +0100

    Making title text a bit more responsive

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -4,12 +4,6 @@ export const Bottom = () => {
     return (
         <div
             className="bottom"
-            style={{
-                position: 'fixed',
-                bottom: 0,
-                right: 0,
-                zIndex: 100,
-            }}
         >
             <h1>WILLIAMS</h1>
         </div>
